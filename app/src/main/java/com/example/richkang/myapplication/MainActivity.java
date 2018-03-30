a2a2dca4c1162be0c9cf0c5fcab12913e5638cb3
HiphopManB <RichManMaster@163.com>
2018-03-30 15:09:18 +0800

    add notes

--- app/src/main/java/com/example/richkang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/richkang/myapplication/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //在这里做个修改  比如 康家帅是个傻逼 
     }
 }
